631c980ac99bf91231983763cbefe44012c77d6b
Will Schurman <wschurman@gmail.com>
2022-04-13 17:28:31 -0700

    Address comment

--- expo-updates-server/jest.config.js
+++ expo-updates-server/jest.config.js
@@ -13,7 +13,6 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
