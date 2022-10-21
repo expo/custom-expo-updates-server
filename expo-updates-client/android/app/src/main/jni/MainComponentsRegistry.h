68e61da78826ead144327e864691217922ef1a6a
Will Schurman <wschurman@expo.io>
2022-10-20 20:48:14 -0700

    client: upgrade to sdk v46 (#14)

--- expo-updates-client/android/app/src/main/jni/MainComponentsRegistry.h
+++ expo-updates-client/android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/expoupdatesclient/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/test/expoupdatesclient/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
