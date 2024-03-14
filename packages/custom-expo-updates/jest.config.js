3a9af925718965660dfa063d8f77bab26b55bf4a
Ty Rauber <tyrauber@mac.com>
2024-03-14 10:53:55 +0100

    fix: root code-signing and updates

--- packages/custom-expo-updates/jest.config.js
+++ packages/custom-expo-updates/jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
 
 process.env = Object.assign(process.env, {
   HOSTNAME: "http://localhost:3000",
-  PRIVATE_KEY_PATH: "code-signing/private-key.pem",
-  UPDATES_ASSET_PATH: "support/updates/",
+  PRIVATE_KEY_PATH: "../../code-signing/private-key.pem",
+  UPDATES_ASSET_PATH: "../../updates/",
 });
