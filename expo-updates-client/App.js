d48a73c58b47ec066b7beca51f4fb437112e7d61
Quinlan Jung <quin@expo.io>
2025-09-17 13:41:17 -0700

    update 3

--- expo-updates-client/App.js
+++ expo-updates-client/App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Constants from 'expo-constants';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Constants from "expo-constants";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app 3!</Text>
+      <Text>Open up App.js to start working on your app 4!</Text>
       <Text>{Constants.expoConfig.name}</Text>
-      <Image source={require('./assets/favicon.png')} />
+      <Image source={require("./assets/favicon.png")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
